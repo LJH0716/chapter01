610595969a135eead71f0d06fd3c3d26be1ecd93
LJH0716 <jhmk0805@naver.com>
2022-04-13 16:08:57 +0900

    2022-04-13

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,70 @@
+/*작성자: 이정화
+작성일: 2022-04-13
+설명: 나이 기록 프로그램*/
+
+package com.javaex.ex01;
+
+public class Ex01 {
+
+		public static void main(String[] args) {
+		
+			/*
+			int myAge; //변수선언
+			myAge = 25; //초기화
+			*/
+			
+			int myAge = 25; //변수선언 + 초기화
+			System.out.println(myAge);
+			 
+			/*//메모리의 값을 출력하는 메소드 
+			System.out.println(myAge);
+			
+			myAge =26; 
+			System.out.println(myAge);
+			*/
+			
+			byte no;
+			no = 127;
+			System.out.println(no);
+			
+			//long형은 숫자 뒤에 L 붙여줄 것
+			long no2;
+			no2 = 2147483648L;
+			System.out.println(no2);
+			
+			
+			////////////////////////////////////////////
+			//변수선언, 초기화
+			////////////////////////////////////////////
+			
+			//변수 여러 개일때 변수 선언 후 초기화
+			int var01;
+			int var02;
+			int var03;
+			//int var01, var02, var03;
+			
+			var01 = 10;
+			var02 = 20;
+			var03 = 30;			
+			
+			//변수 여러 개일때 변수 선언 + 초기화
+			int var04 = 10;
+			int var05 = 20;
+			int var06 = 30;
+			//int var04 = 10, var05 = 20, var06 = 30;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}		
+}
---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,31 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+	
+	public static void main(String[] args) {
+	
+		char ch01 = 'A';
+		char ch02 = '안';
+		
+		System.out.println(ch01);
+		System.out.println(ch02);
+		
+		char ch03 = 65;
+		System.out.println(ch03);
+		//문자에 대한 코드값이 있음 ex) A=65 
+		
+		char ch04 = 3;
+		System.out.println(ch04);
+		//숫자만 넣으면 코드표 값으로 나오고, ''처리해줘야 문자로 인식
+		//----------------기본 자료형-----------------
+		
+		String str = "안녕하세요";
+		System.out.println(str);
+		
+		
+		
+		
+		
+	}
+		
+}
