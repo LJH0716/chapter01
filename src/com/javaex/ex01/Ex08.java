d8f0971b1c37704c0f91270d1cc724110d134d56
LJH0716 <jhmk0805@naver.com>
2022-04-14 16:17:26 +0900

    2022-04-14 연산자

---
+++ src/com/javaex/ex01/Ex08.java
@@ -0,0 +1,69 @@
+package com.javaex.ex01;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+	
+		
+		//대입연산자
+		int a = 7;
+		int b = 2;
+		
+		System.out.println("산술연산자");
+		//산술연산자
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b); //정수/정수-->정수(몫)
+		System.out.println(a%b); //나머지
+	
+		//산술연산자 / % 자세히
+		System.out.println("산술연산자 / % 자세히");
+		System.out.println("============");
+		System.out.println(7/2);       //3
+		System.out.println(7.0/2.0);   //3.5
+		System.out.println(7%2);       //1
+		System.out.println(7.0%2.0);   //1.0
+		System.out.println("===========");
+		
+		//부호 연산자
+		int var = -3;
+		int pVar = +var;
+		int mVar = -var;
+	
+		System.out.println(var);  
+		System.out.println(pVar);  
+		System.out.println(mVar);  
+	
+		//증감 연산자(++n / n++ 차이점!)
+		
+
+		System.out.println("증감연산자");  
+		System.out.println(a);     //7
+		System.out.println(++a);   //8 ******
+		System.out.println(a);     //8
+		
+		System.out.println(b);     //2
+		System.out.println(--b);   //1 (2-->1)
+		System.out.println(b);     //1
+		
+		
+		System.out.println(a);     //8
+		System.out.println(a++);   //8 ******
+		System.out.println(a);     //9
+	
+		System.out.println(b);     //1
+		System.out.println(b--);   //1
+		System.out.println(b);     //0
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/com/javaex/ex01/Ex09.java
@@ -0,0 +1,24 @@
+package com.javaex.ex01;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+	
+		//관계 연산자
+		int n1 = 5;
+		int n2 = 3;
+		
+		System.out.println(n1<n2);  //5<3
+		System.out.println(n1>n2);  //5>3
+		
+		System.out.println(n1<=n2); 
+		System.out.println(n1>=n2); 
+		
+		System.out.println(n1==n2); 
+		System.out.println(n1!=n2); 
+		
+		boolean result = n1<n2;
+		System.out.println(result);
+	
+	}
+}
